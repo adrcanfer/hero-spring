e7acc0caaddccd984ceb13b96b5aa90ec557324c
Adrián Cantón <acanton@deloitte.es>
2022-07-06 20:22:06 +0200

    Added api doc

--- src/main/java/com/adrcanfer/hero/controller/HeroController.java
+++ src/main/java/com/adrcanfer/hero/controller/HeroController.java
@@ -7,7 +7,6 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,12 +15,13 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.adrcanfer.hero.exception.CustomException;
 import com.adrcanfer.hero.model.Hero;
 import com.adrcanfer.hero.service.HeroService;
 
-@Controller
+@RestController
 @RequestMapping("/hero")
 public class HeroController {
 	
